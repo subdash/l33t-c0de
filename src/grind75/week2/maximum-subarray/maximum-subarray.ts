54202000a796e973dd462c446f36789f606970eb
subdash <dhrs.dev@gmail.com>
2023-04-03 17:25:26 -0500

    Max subarray

---
+++ src/grind75/week2/maximum-subarray/maximum-subarray.ts
@@ -0,0 +1,55 @@
+onst maxSubArray = (nums: number[]): number => {
+    let localMax = nums[0];
+    let finalMax = localMax;
+    for (const num of nums.slice(1)) {
+        // If the current number is greater than the previous max,
+        // then it is the new local max.
+        localMax = Math.max(num, localMax + num);
+        if (localMax > finalMax) {
+            finalMax = localMax;
+        }
+    }
+    return finalMax;
+}
+
+/*
+// Works for all but 10 test cases where we get this error:
+terminate called after throwing an instance of 'std::bad_alloc'
+  what():  std::bad_alloc
+function maxSubArray(nums: number[]): number {
+    if (nums.length === 1) {
+        return nums[0];
+    }
+
+    const sums: Map<string, number> = new Map();
+    let max = nums[0];
+
+    for (let i = 0; i < nums.length; ++i) {
+        for (let j = i; j < nums.length; ++j) {
+            let newSum;
+            switch (j - i) {
+                case 0:
+                    newSum = nums[i];
+                    break;
+                case 1:
+                    newSum = nums[i] + nums[j];
+                    break;
+                default:
+                    newSum = sums.get(mapKey(i, j - 1)) + nums[j];
+                    break;
+            }
+            
+            sums.set(mapKey(i, j), newSum);
+            if (newSum > max) {
+                max = newSum;
+            }
+        }
+    }
+    
+
+    return max;
+};
+
+const mapKey = (x: number, y: number): string => `${x}:${y}`;
+*/
+
