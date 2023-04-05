c496e01880c770f829d9a08aee3de4ef1044b6b3
subdash <dhrs.dev@gmail.com>
2023-04-05 12:25:39 -0500

    Move dupe solution, update 3sum solution

--- src/grind75/week3/3sum/3sum.ts
+++ src/grind75/week3/3sum/3sum.ts
@@ -1,67 +1,44 @@
 function threeSum(nums: number[]): number[][] {
-    nums.sort((a, b) => a - b);
+    /*
+        Sliding window approach: check each number, skipping duplicates
+        you encounter. When a new number is found, use a sliding window
+        to find sums equal to 0. We'll check all non-duplicate numbers
+        within the window where the lowest number is the one in the loop
+        (i), the next non-duplicate (start) and the last non-duplicate (end).
+    */
+    nums.sort((a, b) => a - b); // sort in ascending order, negatives first
     const triplets = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        // Skip duplicates at start of array
-        if (i > 0 && nums[i - 1] === nums[i]) {
-            continue;
-        }
+    for (let i = 0; i < nums.length; ++i) {
+        // Skip dupes at beginning
+        if (nums[i] === nums[i - 1]) { continue }
+
         let start = i + 1;
         let end = nums.length - 1;
+        while (start < end) {
+            const sum = nums[i] + nums[start] + nums[end];
 
-        while (start < end
-                && nums[i] !== undefined
-                && nums[start] !== undefined
-                && nums[end] !== undefined) {
-            switch (compareToZero(nums[i], nums[start], nums[end])) {
-            case CmpResult.LT:
-                // Sum less than zero, so increment start pointer
-                start++;
-                break;
-            
-            case CmpResult.GT:
-                // Sum greater than zero, decrement end point
-                end--;
-                break;
-
-            case CmpResult.EQ:
-                // Triplet found. Increment start pointer until we have
-                // skipped remaining duplicates,
+            if (sum === 0) {
+                // Triplet found
                 triplets.push([nums[i], nums[start], nums[end]]);
-                start++;
-                while (nums[start] === nums[start - 1]) {
-                    start++;
-                }
 
-                // Decrement start pointer, then increment it until we
-                // skip over all values equal to previous.
-                end--;
-                while (nums[end] === nums[start + 1]) {
-                    end++;
-                }
-                break;
+                // Move start of window forward until you pass dupes
+                ++start;
+                while (nums[start] === nums[start - 1]) { ++start }
+
+                // Move end of window back 1, then move it forward until
+                // you pass dupes at end.
+                --end;
+                while (nums[end] === nums[start + 1]) { ++end }
+            } else if (sum < 0) {
+                // Move start of window forward
+                ++start;
+            } else {
+                // Sum greater than 0, move end of window back
+                --end;
             }
         }
     }
-
     return triplets;
 };
-
-enum CmpResult {
-    GT,
-    LT,
-    EQ
-};
-
-const compareToZero = (x: number, y: number, z: number): CmpResult => {
-    const sum = x + y + z;
-    if (sum < 0) {
-        return CmpResult.LT;
-    }
-    if (sum > 0) {
-        return CmpResult.GT;
-    }
-    return CmpResult.EQ;
-}
 
